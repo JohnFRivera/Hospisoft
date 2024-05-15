0dd81800a6e26b13ebfaef8fe0f707a1d9cb8e57
kevin alzate <kevinalzate10290@gmail.com>
2024-05-14 20:56:02 -0500

    login de los usuarios

--- backend/src/usuario/controller/usuario.controller.js
+++ backend/src/usuario/controller/usuario.controller.js
@@ -1,84 +1,196 @@
-import {pool} from '../../model/MySql.js';
+import { pool } from '../../model/MySql.js';
 
 
 export const getUsuarios = (req, res) => {
 
-    let sql ="SELECT * FROM usuarios "
-    
-    pool.query(sql, (error, data) => {
-      if(!error){
-        res.status(200).send(data)
-      }else{
+  let sql = "SELECT * FROM usuarios "
+
+  pool.query(sql, (error, data) => {
+    if (!error) {
+      res.status(200).send(data)
+    } else {
+      res.status(500).send({
+        title: error.code,
+        message: error.message
+      })
+    }
+  });
+}
+
+export const postUsuarios = (req, res) => {
+  console.log(req.body);
+  let frmData = {
+    id: req.body.id,
+    identificacion: req.body.identificacion,
+    usuario: req.body.usuario,
+    email: req.body.email,
+    contraseña: req.body.contraseña,
+    rol: req.body.rol
+
+  }
+
+  pool.query("INSERT INTO usuarios set ?", frmData, (error, data) => {
+    if (!error) {
+      res.status(200).send({
+        title: "Agregado exitosamente",
+        message: `Usuario <b class="text-primary">${frmData.nombre} ${frmData.apellidos}</b> agregado correctamente.`
+      })
+    } else {
+      res.status(500).send({
+        title: error.code,
+        message: error.message
+      })
+    }
+  })
+}
+
+export const putUsuarios = (req, res) => {
+  let id = req.params.id;
+  let frmData = {
+    identificacion: req.body.identificacion,
+    usuario: req.body.usuario,
+    email: req.body.email,
+    contraseña: req.body.contraseña,
+    rol: req.body.rol
+
+  }
+  pool.query("UPDATE usuarios set ? WHERE id= ?", [frmData, id], (error, data) => {
+    if (!error) {
+      res.status(200).send("editado")
+    } else {
+      res.status(500).send({
+        title: error.code,
+        message: error.message
+      })
+    }
+  })
+}
+
+
+export const deleteUsuario = (req, res) => {
+  let id = req.params.id;
+  let sql = "DELETE from usuarios WHERE id =" + id;
+  pool.query(sql, (error, data) => {
+    if (!error) {
+      res.status(200).send("usuario eliminado con exito")
+    } else {
+      res.status(500).send({
+        title: error.code,
+        message: error.message
+      })
+    }
+  })
+}
+
+const prueba = [
+  {
+    page: "dashboard",
+    icon: "bi-graph-up",
+    text: "Dashboard",
+  },
+  {
+    page: "usuarios",
+    icon: "bi-person",
+    text: "Usuarios",
+  },
+  {
+    page: "Usuario/separar_cita",
+    icon: "bi-calendar-plus",
+    text: "Separar Cita",
+  },
+  {
+    page: "Medico/citas",
+    icon: "bi-calendar2-minus",
+    text: "Citas",
+  },
+  {
+    page: "historias_clinicas",
+    icon: "bi-file-text",
+    text: "Historias Clínicas",
+  },
+  {
+    page: "Secretaria/generar_horario_especialista",
+    icon: "bi-calendar-week",
+    text: "Generar Horario",
+  },
+  {
+    page: "Dispensario/medicinas",
+    icon: "bi-prescription2",
+    text: "Medicinas",
+  },
+  {
+    page: "crear_campaña",
+    icon: "bi-chat-dots",
+    text: "Crear Campaña",
+  },
+];
+
+export const loginUser = (req, res) => {
+  let email = req.body.email;
+  let contraseña = req.body.contraseña;
+  let identificacion = req.body.identificacion
+  let sql = "SELECT usuario,email,contraseña,rol,identificacion FROM usuarios WHERE email = ?"
+  pool.query(sql, email, (error, data) => {
+    if (!error) {
+      res.status(200).send({
+        email: data[0].email
+      })
+      if (data[0].contraseña == contraseña) {
+        if (data[0].identificacion == identificacion) {
+         let btn = []
+         switch (data[0].rol) {
+          case "Administrador":
+           btn = prueba
+
+            break;
+          case "Usuario" :
+          btn= [{
+            page: "Usuario/separar_cita",
+            icon: "bi-calendar-plus",
+            text: "Separar Cita",
+          }]
+          break;
+
+          case "Pacientes":
+            btn = [
+              {
+                page: "Usuario/separar_cita",
+                icon: "bi-calendar-plus",
+                text: "Separar Cita",
+            }
+
+            ]
+            case "Secretaria" :
+         btn = [
+          {
+            page: "Secretaria/generar_horario_especialista",
+            icon: "bi-calendar-week",
+            text: "Generar Horario",
+        }
+         ]
+          default:
+            break;
+         }
+
+        }else{
+          res.status(500).send({
+            title: "Error de identificacion",
+            message: "no existe la identificaion del usuario"
+          })
+        }
+      } else {
         res.status(500).send({
-          title:error.code,
-            message:error.message
+          title: "Error de contraseña",
+          message: "no existe la contraseña"
         })
       }
-    });
-  }
 
-  export const postUsuarios =  (req, res) => {
-   console.log(req.body);
-    let frmData={
-        id: req.body.id,
-        identificacion: req.body.identificacion,
-        usuario: req.body.usuario, 
-        email: req.body.email,
-        contraseña: req.body.contraseña,
-        rol: req.body.rol
-        
-    } 
-
-    pool.query("INSERT INTO usuarios set ?",frmData,(error,data)=>{
-        if(!error){
-            res.status(200).send({
-              title: "Agregado exitosamente",
-              message: `Usuario <b class="text-primary">${frmData.nombre} ${frmData.apellidos}</b> agregado correctamente.`
-            })
-        }else{
-            res.status(500).send({
-              title:error.code,
-                message:error.message
-            })
-        } 
-    }) 
-  }
-
-  export const putUsuarios =  (req, res) => {
-    let id = req.params.id;
-    let frmData={
-        identificacion: req.body.identificacion,
-        usuario: req.body.usuario, 
-        email: req.body.email,
-        contraseña: req.body.contraseña,
-        rol: req.body.rol
-        
-    } 
-    pool.query("UPDATE usuarios set ? WHERE id= ?",[frmData,id],(error,data)=>{
-        if(!error)
-        {
-            res.status(200).send("editado")
-        }else{
-            res.status(500).send({
-              title:error.code,
-                message:error.message
-            })
-        }
-    })
-  }
-
-
-  export const deleteUsuario= (req, res) => {
-    let id = req.params.id;
-    let sql = "DELETE from usuarios WHERE id ="+id;
-    pool.query(sql,(error,data)=>{
-        if(!error){
-            res.status(200).send("usuario eliminado con exito")
-        }else{
-            res.status(500).send({
-              title:error.code,
-            message:error.message
-        })}
-    })
-  }
-
+    }else {
+      res.status(500).send({
+        title: "error de correo",
+        message: "no se encuentra el correo"
+      })
+    }
+      
+  })
+}
--- backend/src/usuario/routes/usuario.routes.js
+++ backend/src/usuario/routes/usuario.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getUsuarios, postUsuarios, putUsuarios, deleteUsuario} from "../controller/usuario.controller.js";
+import { getUsuarios, postUsuarios, putUsuarios, deleteUsuario, loginUser} from "../controller/usuario.controller.js";
 const usuario = Router()
 
 usuario.get("/usuario/listing", getUsuarios)
 usuario.post("/usuario/add", postUsuarios)
 usuario.put("/usuario/edit/:id", putUsuarios)
 usuario.delete("/usuario/delete/:id", deleteUsuario)
+usuario.post("/usuario/login", loginUser)
 
 export default usuario
